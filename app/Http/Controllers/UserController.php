5472afd7992d493f09e0ec3e2324ac0441f432b7
AlexVelezLl <alanvele@espol.edu.ec>
2021-09-05 14:24:47 -0500

    Add query to know if I follow a specific user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,104 +8,114 @@ use Illuminate\Support\Facades\Crypt;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return User::all();
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return User::all();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $encrypted =  crypt::encryptString($request->input('password'));
-        $request->merge(['password' => $encrypted]);
-        $user = User::create($request->all());
-        return $user;
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $encrypted =  crypt::encryptString($request->input('password'));
+    $request->merge(['password' => $encrypted]);
+    $user = User::create($request->all());
+    return $user;
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(int $id)
-    {
-        $user = User::find($id);
-        return collect($user->toArray())
-            ->only(['username', 'name', 'image'])
-            ->all();
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\Response
+   */
+  public function show(int $id)
+  {
+    $user = User::find($id);
+    return collect($user->toArray())
+      ->only(['username', 'name', 'image'])
+      ->all();
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(User $user)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, int $id)
-    {
-        return User::find($id)->update($request->all());
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, int $id)
+  {
+    return User::find($id)->update($request->all());
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(int $id){
-        return User::destroy($id);
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\User  $user
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(int $id)
+  {
+    return User::destroy($id);
+  }
 
-    public function followUser($follows_user){
-        $user = (object) auth()->user();
-        $user->following()->attach($follows_user);
-        return [
-          "message" => "Ha empezado a seguir a este usuario."
-        ];
-    }
+  public function followingUser($id_user)
+  {
+    $user = (object) auth()->user();
+    $following = $user->following()->where('follows_user', $id_user)->first();
+    return ['following' => !!$following];
+  }
 
-    public function unfollowUser($follows_user){
-        $user = (object) auth()->user();
-        $user->following()->detach($follows_user);
-        return [
-          "message" => "Ha dejado de seguir a este usuario."
-        ];
-    }
+  public function followUser($follows_user)
+  {
+    $user = (object) auth()->user();
+    $user->following()->attach($follows_user);
+    return [
+      "message" => "Ha empezado a seguir a este usuario."
+    ];
+  }
 
-    /**public function profile()
+  public function unfollowUser($follows_user)
+  {
+    $user = (object) auth()->user();
+    $user->following()->detach($follows_user);
+    return [
+      "message" => "Ha dejado de seguir a este usuario."
+    ];
+  }
+
+  /**public function profile()
     {
         $user = User::find(1);
         return collect($user->toArray())
